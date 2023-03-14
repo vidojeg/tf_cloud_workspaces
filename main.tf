086cd8848d2c8737604537bca4c723ed182a5ca9
Vidoje Gavrilovic <vidoje.gavrilovich@gmail.com>
2023-03-15 00:38:28 +0100

    move to module

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-resource "tfe_variable_set" "test" {
-  name         = "sandbox-test"
-  description  = "Variable set applied to all workspaces."
-  global       = true
-  organization = lookup(var.tfe_org, var.environment_code)
+module "workspaces" {
+  source = "./modules/workspaces"
+
+  tfe_org       = lookup(var.tfe_org, var.environment_code)
+  global_varset = var.environment_code
+  # global_variables = lookup(var.global_variables, var.environment_code)
+  # vcs              = lookup(var.vcs, var.environment_code)
 }
