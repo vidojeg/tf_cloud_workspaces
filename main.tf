597644c24485bbebbf2df9f5d107c9e81fb7dcbf
Vidoje Gavrilovic <vidoje.gavrilovich@gmail.com>
2023-03-15 00:19:57 +0100

    remove module

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
-module "workspaces" {
-  source = "./modules/workspaces"
-
-  tfe_org       = lookup(var.tfe_org, var.environment_code)
-  global_varset = var.environment_code
-  # global_variables = lookup(var.global_variables, var.environment_code)
-  # vcs              = lookup(var.vcs, var.environment_code)
+resource "tfe_variable_set" "test" {
+  name         = "sandbox-test"
+  description  = "Variable set applied to all workspaces."
+  global       = true
+  organization = lookup(var.tfe_org, var.environment_code)
 }
